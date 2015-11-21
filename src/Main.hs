de8ce1b6250fcbf9d2fd7de5d22282955ffefd1c
Amir Razmjou <arazmj@gmail.com>
2015-11-21 17:37:42 -0500

    Fixed warnnings

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,11 @@
 data Tree a = Leaf Char | Branch (Tree a) (Tree a)
 
+main :: IO ()
 main = interact (unlines . showResult . readTestCase . lines)
 
-readTestCase :: [t] -> (t, [t])
+readTestCase :: [String] -> (String, [String])
 readTestCase (x:lx) = (x, lx)
+readTestCase _     =  error "Input must be more than 1 line."
 
 showResult :: (String, [String]) -> [String]
 showResult (encoding, messages) = map (decodeMessage encodingTree) messages
@@ -14,15 +16,16 @@ encodeTree ('*':encoding)  =
     let (encodingLeft, left)  = encodeTree encoding
         (encodingRight, right) = encodeTree (tail encodingLeft)
     in (encodingRight, Branch left right)
-encodeTree (encoding)  = encoding, Leaf (head encoding)
+encodeTree (encoding)  = (encoding, Leaf (head encoding))
 
 decodeChar :: String -> Tree t -> (Char, String)
 decodeChar rest     (Leaf c)         = (c, rest)
 decodeChar ('0':xb) (Branch left _)  = decodeChar xb left
 decodeChar ('1':xb) (Branch _ right) = decodeChar xb right
+decodeChar _  _                      = error "The encoding is for the message: "
 
 decodeMessage :: Tree t -> String -> String
-decodeMessage tree []      = []
+decodeMessage _ []      = []
 decodeMessage tree message =
     let (c, rest) = decodeChar message tree
     in c:decodeMessage tree rest
